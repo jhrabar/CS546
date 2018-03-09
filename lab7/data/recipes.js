b025f10fe225f1b68ecca440a721a6d071bc60bf
John <jhrabar@stevens.edu>
2018-03-08 20:25:44 -0500

    started lab 7

---
+++ lab7/data/recipes.js
@@ -0,0 +1,182 @@
+const mongoCollections = require("../config/mongoCollections");
+const recipes = mongoCollections.recipes;
+const uuid = require("uuid/v4");
+
+
+async function getAllRecipes() {
+  try {
+    let recipes = await recipes();
+    let arrayRecipes = await recipes.find({}).toArray();
+    let finalArray = [];
+    for(let i = 0; i < arrayRecipes.length; i++){
+      finalArray.push({_id: arrayRecipes[i]._id, title: arrayRecipes[i].title});
+    }
+    return finalArray;
+  }catch(error) {
+    console.log(error.message);
+  }
+}
+
+async function getRecipeByID(id) {
+  try {
+    if(typeof id !== 'string'){
+      throw Error("id is invalid");
+    }
+    let recipes = await recipes();
+    let recipe = await recipes.findOne({_id: id});
+    return recipe;
+  }
+  catch(error){
+    console.log(error.message);
+  }
+}
+
+async function postRecipe(newRecipe){
+  try {
+    if(!newRecipe.title || typeof newRecipe.title !== "string"){
+      throw Error("Title is not valid");
+    }
+    if(!newRecipe.ingredients || Array.isArray(newRecipe.ingredients) == false){
+      throw Error("Ingredients are not valid");
+    }
+    if(!newRecipe.steps || Array.isArray(newRecipe.steps) == false){
+      throw Error("Steps are not valid");
+    }
+    newRecipe.ingredients.forEach((ingredient) => {
+      if(!ingredient.name || typeof ingredient.name !== "string"){
+        throw Error("Each ingredient must have a name");
+      }
+      if(!ingredient.amount || typeof ingredient.amount !== "string"){
+        throw Error("Each ingredient must have an amount");
+      }
+    })
+    newRecipe.steps.forEach((step) => {
+      if(!step || typeof step !== "string"){
+        throw Error(`Step ${step} is not valid`);
+      }
+    })
+    let object ={
+      _id: uuid(),
+      title: title,
+      ingredients: ingredients,
+      steps: steps
+    }
+    let recipes = await recipes();
+    await recipes.insertOne(object);
+    return await recipes.findOne({_id: object._id});
+  } catch(error){
+    console.log(error.message);
+  }
+
+}
+
+async function putRecipe(id, updatedRecipe){
+  try {
+    if(!updatedRecipe.title || typeof updatedRecipe.title !== "string"){
+      throw Error("Title is not valid");
+    }
+    if(!updatedRecipe.ingredients || Array.isArray(updatedRecipe.ingredients) == false){
+      throw Error("Ingredients are not valid");
+    }
+    if(!updatedRecipe.steps || Array.isArray(updatedRecipe.steps) == false){
+      throw Error("Steps are not valid");
+    }
+    if(updatedRecipe.hasOwnProperty("_id") == true){
+      throw Error("New Recipe should not have an id");
+    }
+
+    updatedRecipe.ingredients.forEach((ingredient) => {
+      if(!ingredient.name || typeof ingredient.name !== "string"){
+        throw Error("Each ingredient must have a name");
+      }
+      if(!ingredient.amount || typeof ingredient.amount !== "string"){
+        throw Error("Each ingredient must have an amount");
+      }
+    })
+    updatedRecipe.steps.forEach((step) => {
+      if(!step || typeof step !== "string"){
+        throw Error(`Step ${step} is not valid`);
+      }
+    })
+
+    if(typeof id !== 'string'){
+      throw Error("id is invalid");
+    }
+    let recipes = await recipes();
+
+    updatedRecipe["_id"] = id;
+    const update = await recipes.replaceOne({_id: id}, updatedRecipe, true);
+    return await recipes.findOne({_id: id});
+  } catch(error) {
+    console.log(error.message);
+  }
+}
+
+async function patchRecipe(id, updatedRecipe){
+  try {
+    let toUpdate = {};
+    if(updatedRecipe.title){
+      if(typeof updatedRecipe.title !== "string"){
+        throw Error("Title is not valid");
+    }
+    toUpdate.title = updatedRecipe.title;
+  }
+    if(updatedRecipe.ingredients){ 
+      if(Array.isArray(updatedRecipe.ingredients) == false){
+        throw Error("Ingredients are not valid");
+    }
+    updatedRecipe.ingredients.forEach((ingredient) => {
+      if(!ingredient.name || typeof ingredient.name !== "string"){
+        throw Error("Each ingredient must have a name");
+      }
+      if(!ingredient.amount || typeof ingredient.amount !== "string"){
+        throw Error("Each ingredient must have an amount");
+      }
+    })
+    toUpdate.ingredients = updatedRecipe.ingredients;
+  }
+    if(updatedRecipe.steps){
+       if(Array.isArray(updatedRecipe.steps) == false){
+        throw Error("Steps are not valid");
+    }
+    updatedRecipe.steps.forEach((step) => {
+      if(!step || typeof step !== "string"){
+        throw Error(`Step ${step} is not valid`);
+      }
+    })
+    toUpdate.steps = updatedRecipe.steps;
+
+  }
+    if(typeof id !== 'string'){
+      throw Error("id is invalid");
+    }
+    let recipes = await recipes();
+
+    const update = await recipes.findOneAndUpdate({_id: id}, { $set: toUpdate});
+    return await recipes.findOne({_id: id});
+  } catch(error) {
+    console.log(error.message);
+  }
+}
+
+async function removeRecipe(id){
+  try {
+    if(typeof id !== 'string'){
+      throw Error("id is not valid");
+    }
+
+    let recipes = await recipes();
+    await recipes.deleteOne({_id: id});
+  } catch(error){
+    console.log(error.message);
+  }
+}
+
+module.exports = {
+  removeRecipe,
+  patchRecipe,
+  putRecipe,
+  postRecipe,
+  getAllRecipes,
+  getRecipeByID
+}
---
+++ lab7/routes/recipes.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const router = express.Router();
+const data = require("../data");
+const recipeData = data.recipes;
+
+router.get("/:id", async (req, res) => {
+  try {
+    const recipe = await recipeData.getRecipeById(req.params.id);
+    res.json(recipe);
+  } catch (e) {
+    res.status(404).json({ message: "not found!" });
+  }
+});
+
+router.get("/", async (req, res) => {
+  try {
+    const recipeList = await recipeData.getAllRecipes();
+    res.json(recipeList);
+  } catch (e) {
+    // Something went wrong with the server!
+    res.status(500).send();
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let recipe = await recipeData.postRecipe(req.body);
+    res.json(recipe);
+  } catch(e){
+    res.status(500).send();
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    let updatedRecipe = await recipeData.putRecipe(req.params.id, req.body);
+    res.json(updatedRecipe);
+  } catch(e){
+    res.status(400).json(err);
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    let updatedRecipe = await recipeData.postRecipe(req.params.id, req.body);
+    res.json(updatedRecipe);
+  } catch(e){
+    res.status(400).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        await recipeData.removeRecipe(req.params.id);
+        res.status(200).json({message : "item deleted"});
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+module.exports = router;
